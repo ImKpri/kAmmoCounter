62c41b3f58d9d01d252bcd1544ab591f2d6754d6
Kpri <122019858+ImKpri@users.noreply.github.com>
2024-05-02 15:43:20 +0200

    Add files via upload

---
+++ AmmoCl.lua
@@ -0,0 +1,36 @@
+
+local function DrawText3DSecond(x,y,z, text)
+    local px, py, pz = table.unpack(GetGameplayCamCoords())
+    local onScreen,_x,_y = World3dToScreen2d(x,y,z)
+    SetTextScale(0.30, 0.30)
+    SetTextFont(4)
+    SetTextProportional(0)
+    SetTextColour(255, 0, 0, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+end
+
+
+local weaponAmmo = 0
+local currentWeapon = ""
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local playerPed = PlayerPedId()
+
+        if IsPedArmed(playerPed, 6) then -- Check if the player is holding a weapon
+            if not IsPlayerFreeAiming(PlayerId()) then
+                local boneIndex = GetPedBoneIndex(playerPed, 57005) -- Right Hand bone
+                local coords = GetWorldPositionOfEntityBone(playerPed, boneIndex)
+                local _, _, z = table.unpack(coords)
+                local ammoInClip, ammoInClipMax = GetAmmoInClip(playerPed, GetSelectedPedWeapon(playerPed))
+                DrawText3DSecond(coords.x, coords.y, coords.z + 0.2, "~b~" .. ammoInClipMax .. " / 250")
+            end
+        end
+    end
+end)
+
+
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+version '1.0'
+author 'Kpri'
+description 'kAmmoCounter'
+
+
+client_scripts {
+
+	'AmmoCl.lua'
+
+}
+
+lua54 'yes'
+game 'gta5'
+fx_version 'cerulean'
